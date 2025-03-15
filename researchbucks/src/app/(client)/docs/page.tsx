9c9e8959fd1dce68183c805bb844c968ecb370cb
Isuru-raketman <isuru.ko@raketman.io>
2025-03-15 13:48:13 +0530

    feat: project initiate

--- researchbucks/src/app/(client)/docs/page.tsx
+++
@@ -1,9 +0,0 @@
-import Container from "@/components/layout/container";
-
-export default function DocsPage() {
-  return (
-    <Container>
-      <h1>Docs Page</h1>
-    </Container>
-  );
-}
--- researchbucks/src/hooks/use-nav-routes.ts
+++
@@ -1,9 +0,0 @@
-import { filterMenuItems } from "@/lib/client/routes";
-import { navMenues } from "@/data/routes/client-menu";
-
-export function useNavRoutes() {
-  const navBarRoutes = filterMenuItems(navMenues, "footer");
-  const footerRoutes = filterMenuItems(navMenues, "navbar");
-
-  return { navBarRoutes, footerRoutes };
-}
