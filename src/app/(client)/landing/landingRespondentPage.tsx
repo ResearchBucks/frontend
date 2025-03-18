283f8873a46a8188a47ecd62237eb5082280257d
Yadeeshani <syadeeshani@gmail.com>
2025-03-18 17:31:03 +0530

    feat:respondent section content changed

--- src/app/(client)/landing/landingRespondentPage.tsx
+++ src/app/(client)/landing/landingRespondentPage.tsx
@@ -3,7 +3,7 @@ import { homeRespondent } from "@/assests/assests";
 
 export default function LandingRespondentPage() {
   return (
-    <div className="grid grid-cols-2 w-full max-w-screen px-6">
+    <div className="grid grid-cols-2 gap-8 w-full max-w-screen px-6">
       {/* Image Container */}
       <div className="flex justify-center items-center relative "> 
         <Image
@@ -16,7 +16,21 @@ export default function LandingRespondentPage() {
       </div>
       
       {/* Content Container */}
-      <div className="">
+      <div className="flex flex-col gap-12 justify-center">
+        <h1 className="text-4xl font-bold">Earn Money Online</h1>
+
+        <div className="flex flex-col gap-3">
+            <h2 className="text-xl font-bold">As Respondent...</h2>
+            <p className="text-sm italic tracking-wider">Your Feedback Matters, And We Reward It!</p>
+
+            <p className="text-sm tracking-widest text-justify">Earn rewards by sharing your opinions on ResearchBucks! 
+                Participate in surveys that interest you and get paid for your feedback. 
+                Our secure platform offers flexibility and privacy, allowing you to choose 
+                surveys based on your preferences. Help shape important decisions while 
+                earning rewards for your time.</p>
+            <h3 className=" text-sm italic tracking-wider text-gray-600">Sign Up and Start Earning Today!</h3>
+        </div>
+
 
       </div>
     </div>
