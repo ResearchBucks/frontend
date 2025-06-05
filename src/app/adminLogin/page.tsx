1cf156c4fe86103a7d7e8e51c2c47351299b1da9
Yadeeshani <syadeeshani@gmail.com>
2025-06-05 12:20:29 +0530

    feat:chnaged the user stylr handling

---
+++ src/app/adminLogin/page.tsx
@@ -0,0 +1,10 @@
+export default function adminLogin(){
+    return(
+        <div className="bg-mainlight w-screen min-h-screen  items-center flex justify-center">
+            <div className="bg-white">
+                
+            </div>
+
+        </div>
+    )
+}
