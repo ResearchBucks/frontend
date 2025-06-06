9450df59c89b51944bcc06c30ca7f7b69de9fdc9
Yadeeshani <syadeeshani@gmail.com>
2025-06-06 08:45:26 +0530

    feat:changed the update section

--- src/app/adminLogin/page.tsx
+++ src/app/adminLogin/page.tsx
@@ -1,10 +1,17 @@
+import { homeRespondent,logo } from "@/assests/assests"
+
 export default function adminLogin(){
     return(
         <div className="bg-mainlight w-screen min-h-screen  items-center flex justify-center">
-            <div className="bg-white">
-                
-            </div>
+            <div className="bg-white grid grid-cols-2 p-4">
+                <div className="flex flex-col w-32 ">
+                    <img src={logo} className=""/>
 
+                </div>
+                <div className="">
+                    <img src={homeRespondent} className="rounded-md"/>
+                </div>
+            </div>
         </div>
     )
 }
