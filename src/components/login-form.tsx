1f1fef871f2c00f71d493815f1fd0d88ca23a08a
Isuru-raketman <isuru.ko@raketman.io>
2025-06-07 22:13:49 +0530

    feat: save user data using redux

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,25 +1,31 @@
 "use client";
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { userloginImg } from "@/assests/assests";
-import { loginpwImg } from "@/assests/assests";
 import { useForm } from "react-hook-form";
-import { userLoginSchema } from "@/schema/user/user-details";
-import { userLoginDataTypes } from "@/schema/user/user-details";
+import {
+  userLoginSchema,
+  userLoginDataTypes,
+} from "@/schema/user/user-details";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useAppDispatch } from "@/lib/redux/hooks";
 import CustomAxios from "@/app/api/CustomAxios";
-import { setAccessToken } from "@/lib/redux/authSlice";
+import {
+  setAccessToken,
+  setUserEmail,
+  setUserID,
+  setUserRole,
+} from "@/lib/redux/authSlice";
 import { useState } from "react";
 import { IoEye, IoEyeOff } from "react-icons/io5";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 type userLoginRes = {};
 export function LoginForm({ onSwitch }: { onSwitch: () => void }) {
   const dispatch = useAppDispatch();
   const [showPassword, setShowPassword] = useState(false);
-  const [userLoginType, setUserLoginType] = useState<string>("researcher")
+  const [userLoginType, setUserLoginType] = useState<string>("researcher");
+  const router = useRouter();
 
   const {
     register,
@@ -32,102 +38,126 @@ export function LoginForm({ onSwitch }: { onSwitch: () => void }) {
   //the login submit function for user
   const onsubmit = async (data: userLoginDataTypes) => {
     try {
-     const endpoint= userLoginType === "researcher" ? "researcher/auth/login" :"respondent/auth/login"
-     const res = await CustomAxios.post(endpoint, data);
+      const endpoint =
+        userLoginType === "researcher"
+          ? "researcher/auth/login"
+          : "respondent/auth/login";
+      const res = await CustomAxios.post(endpoint, data);
       if (res.status === 200) {
         dispatch(setAccessToken(res.data.data.token));
+        dispatch(setUserRole(res.data.data.roles[0]));
+        dispatch(setUserEmail(res.data.data.email));
+        dispatch(setUserID(res.data.data.id));
+        document.cookie = `userRole=${res.data.data.roles[0]}; path=/`;
+
         console.log(res.data);
+        toast.success("Login Successful");
+        router.push("/admin/dashboard");
       }
     } catch (err) {
+      toast.error("Login Failed. Please check your credentials.");
       console.log(err);
     }
   };
 
   return (
     <>
-    <div className="flex flex-col">
-       <p className="text-base font-medium text-center">Login to ResearchBucks as</p>
-      <div className="flex flex-row justify-between mx-auto gap-12 text-sm">
-        <div className="flex flex-row items-center gap-2">
+      <div className="flex flex-col">
+        <p className="text-base font-medium text-center">
+          Login to ResearchBucks as
+        </p>
+        <div className="flex flex-row justify-between mx-auto gap-12 text-sm">
+          <div className="flex flex-row items-center gap-2">
+            <Input
+              type="checkbox"
+              className="w-4 accent-main cursor-pointer"
+              value="researcher"
+              checked={userLoginType === "researcher"}
+              onChange={() => setUserLoginType("researcher")}
+            />
+            Researcher
+          </div>
+          <div className="flex flex-row items-center gap-2">
+            <Input
+              type="checkbox"
+              className="w-4 accent-main cursor-pointer"
+              value="respondent"
+              checked={userLoginType === "respondent"}
+              onChange={() => setUserLoginType("respondent")}
+            />
+            Respondent
+          </div>
+        </div>
+      </div>
+
+      <form
+        onSubmit={handleSubmit(onsubmit)}
+        className="flex flex-col gap-4 pt-3 text-sm"
+      >
+        {/* the input field for email */}
+        <div>
+          <label>Email</label>
           <Input
-            type="checkbox"
-            className="w-4 accent-main cursor-pointer"
-            value="researcher"
-            checked={userLoginType === "researcher"}
-            onChange={() =>setUserLoginType("researcher")}
+            type="email"
+            placeholder="ex: admin@gmail.com"
+            {...register("email")}
+            className={errors.email ? "border border-error" : ""}
           />
-          Researcher
+          {errors.email && (
+            <span className="text-[10px] text-error">
+              {errors.email.message}
+            </span>
+          )}
         </div>
-        <div className="flex flex-row items-center gap-2">
-        <Input
-          type="checkbox"
-          className="w-4 accent-main cursor-pointer"
-          value="respondent"
-          checked={userLoginType === "respondent"}
-          onChange={() =>setUserLoginType("respondent")}
-        />
-        Respondent       
-      </div>
-      </div>
-    </div>
 
-    <form onSubmit={handleSubmit(onsubmit)} className="flex flex-col gap-4 pt-3 text-sm">
-      {/* the input field for email */}
-      <div>
-        <label>Email</label>
-        <Input
-          type="email"
-          placeholder="ex: admin@gmail.com"
-          {...register("email")}
-          className={errors.email ? "border border-error" : ""}
-        />
-        {errors.email && (
-          <span className="text-[10px] text-error">{errors.email.message}</span>
-        )}
-      </div>
+        {/* the input field for password */}
+        <div className="flex flex-col relative">
+          <label className="text-sm">Password</label>
+          <Input
+            type={showPassword ? "text" : "password"}
+            placeholder="*******"
+            {...register("password")}
+            className={errors.password ? "border border-error" : ""}
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+            className="absolute right-3 top-1/2 -translate-y-1 text-text hover:text-gray-700 focus:outline-none cursor-pointer"
+          >
+            {showPassword ? <IoEyeOff size={18} /> : <IoEye size={18} />}
+          </button>
+          {errors.password && (
+            <span className="text-[10px] text-error">
+              {errors.password.message}
+            </span>
+          )}
+          <div className="flex flex-row justify-end pt-1">
+            <p className="text-[.7rem] cursor-pointer hover:text-main hover:font-medium ">
+              Forgot Password ?
+            </p>
+          </div>
+        </div>
 
-      {/* the input field for password */}
-      <div className="flex flex-col relative">
-        <label className="text-sm">Password</label>
-        <Input
-          type={showPassword ? "text" : "password"}
-          placeholder="*******"
-          {...register("password")}
-          className={errors.password ? "border border-error" : ""}
-        />
-        <button
-          type="button"
-          onClick={() => setShowPassword((prev) => !prev)}
-          className="absolute right-3 top-1/2 -translate-y-1 text-text hover:text-gray-700 focus:outline-none cursor-pointer"
+        {/* the submit for login */}
+        <div className="pt-3 w-full">
+          <Button
+            type="submit"
+            variant="login"
+            size="sm"
+            name="Submit"
+            className="w-full cursor-pointer"
+          />
+        </div>
+      </form>
+      <div className="flex flex-row gap-2 justify-center pt-4 text-xs tracking-wide">
+        <p>Don't have an account?</p>
+        <span
+          className="hover:font-medium hover:underline underline-offset-4 cursor-pointer hover:underline-main hover:text-main"
+          onClick={onSwitch}
         >
-          {showPassword ? <IoEyeOff size={18} /> : <IoEye size={18} />}
-        </button>
-        {errors.password && (
-          <span className="text-[10px] text-error">
-            {errors.password.message}
-          </span>
-        )}
-        <div className="flex flex-row justify-end pt-1">
-          <p className="text-[.7rem] cursor-pointer hover:text-main hover:font-medium ">
-            Forgot Password ?
-          </p>
-        </div>
+          SignUp
+        </span>
       </div>
-
-      {/* the submit for login */}
-      <div className="pt-3 w-full">
-        <Button
-          type="submit"
-          variant="login"
-          size="sm"
-          name="Submit"
-          className="w-full cursor-pointer"
-        />
-      </div>
-    </form>
-    <div className="flex flex-row gap-2 justify-center pt-4 text-xs tracking-wide">
-      <p>Don't have an account?</p><span className="hover:font-medium hover:underline underline-offset-4 cursor-pointer hover:underline-main hover:text-main" onClick={onSwitch}>SignUp</span>
-    </div>  
     </>
   );
 }
